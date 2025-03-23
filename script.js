7a4a3c033779d6c562e57a939eec9c7784883186
Strongbubble2017 <balint_tamas2000@yahoo.com>
2025-03-23 15:27:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
-// Cél időpont (pl. március 31. 23:59:59)
+<script>
 const countdownDate = new Date("2025-03-31T23:59:59").getTime();
-
 const countdownElement = document.getElementById("countdown");
 
 const updateCountdown = setInterval(function () {
@@ -10,18 +8,16 @@ const updateCountdown = setInterval(function () {
 
   if (distance < 0) {
     clearInterval(updateCountdown);
-    countdownElement.innerHTML = "Lejárt az idő!";
+    countdownElement.innerHTML = "A meeting elkezdődött!";
     return;
   }
 
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  countdownElement.innerHTML =
-    days + " nap " + hours + " óra " + minutes + " perc " + seconds + " mp";
+  countdownElement.innerHTML = `${days} nap ${hours} óra ${minutes} perc ${seconds} mp`;
 }, 1000);
+</script>
 
